5f7aeb50be5480e708222de317af78804f4a1cf1
mbougar <146754886+mbougar@users.noreply.github.com>
2023-10-31 15:41:51 +0100

    Update ej2_2_3.py

--- src/ej2_2_3.py
+++ src/ej2_2_3.py
@@ -1,15 +1,12 @@
-def comprobar_Numero(numero: str, posTrue: bool):
-    if posTrue == True and numero[0] == "-":
+def comprobar_Numero(numero: str):
+    try:
+        if int(numero) <= 0:
+            return False
+        else:
+            return True
+    except ValueError:
         return False
-    elif posTrue == True and numero.isdecimal() == True:
-        return True
-    elif posTrue == False and numero[0] != "-":
-        return False
-    elif posTrue == False and numero.replace(numero[0],"").isdecimal() == True:
-        return True
-    else:
-        return False
-        
+    
 
 def cadena_Numeros(numero: int):
     numeroStr = ""
@@ -24,7 +21,7 @@ def cadena_Numeros(numero: int):
 
 def main():
     numero = input("Introduzca un número entero positivo: ")
-    while comprobar_Numero(numero, True) != True or numero == "0":
+    while comprobar_Numero(numero) != True:
         numero = input("Error. Introduzca un número entero positivo: ")
     numero = int(numero)
     print(f"Todos los números impares desde 1 hasta {numero} separados por comas son: {cadena_Numeros(numero)}")
