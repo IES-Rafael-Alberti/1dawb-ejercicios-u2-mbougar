efe57e90709a07a17e4e5ecbda281d834e30832f
mbougar <146754886+mbougar@users.noreply.github.com>
2023-10-31 15:42:40 +0100

    Initial commit

---
+++ tests/test_ej2_2_1.py
@@ -0,0 +1,18 @@
+from src.ej2_2_1 import repetir_Palabra
+import pytest
+
+
+@pytest.mark.parametrize(
+    "input_n1, input_n2, expected",
+    [
+        ("hola", 3, "hola hola hola"),
+        ("no", 1, "no"),
+        ("no", 2, "no no"),
+        ("vegetariana", 2, "vegetariana vegetariana"),
+        ("vegetariana", 10, repetir_Palabra("vegetariana", 10))
+    ]
+)
+
+
+def test_repetir_Palabra_params(input_n1, input_n2, expected):
+    assert repetir_Palabra(input_n1, input_n2) == expected
---
+++ tests/test_ej2_2_2.py
@@ -0,0 +1,18 @@
+from src.ej2_2_2 import calcular_Años
+import pytest
+
+
+@pytest.mark.parametrize(
+    "input_n1, expected",
+    [
+        (5, "1-2-3-4-5."),
+        (1, "1."),
+        (2, "1-2."),
+        (20, "1-2-3-4-5-6-7-8-9-10-11-12-13-14-15-16-17-18-19-20."),
+        (10, calcular_Años(10))
+    ]
+)
+
+
+def test_calcular_Años_params(input_n1, expected):
+    assert calcular_Años(input_n1) == expected
